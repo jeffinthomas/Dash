d69095f45d216a76f4f2c82e79c525cee78d5f76
Jeffin Thomas <36228115+jeffinthomas@users.noreply.github.com>
2020-07-09 12:27:54 +0530

    Update dashTabs.py

--- dashTabs.py
+++ dashTabs.py
@@ -31,4 +31,4 @@ def render_content(tab):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port = 8050, host='13.233.98.249')
+    app.run_server(debug=True, port = 8050, host='0.0.0.0')
